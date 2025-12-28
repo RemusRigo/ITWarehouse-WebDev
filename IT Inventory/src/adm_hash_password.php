8ef5b56d936b713fd09a5ecec57f4e3b49940dbc
Remus Rigo <RemusRigo@users.noreply.github.com>
2025-12-28 09:58:25 +0200

    2025-12-28 update 1

--- IT Inventory/src/adm_hash_password.php
+++ IT Inventory/src/adm_hash_password.php
@@ -1,5 +1,12 @@
 <?php
-$password = "E3d3c3r4f4v4";
+//-------------------------------------------------------------------------------------------------
+//   IT Inventory
+//      © 2025 Remus Rigo
+//         2025-12-28
+//   hash password
+
+<?php
+$password = "admin";
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 echo $hashedPassword;
 ?>
